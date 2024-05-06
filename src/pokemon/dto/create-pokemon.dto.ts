3fbc471acac698f31eebfbca225dc70adb42af50
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-05-06 13:51:19 -0600

    finseccion07

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsIn, IsInt, IsNumber, IsPositive, IsString, MIN, Min, MinLength, min } from "class-validator"
+
+export class CreatePokemonDto {
+
+    @IsInt()
+    @Min(1)
+    @IsPositive()
+    no: number
+
+    @IsString()
+    @MinLength(1)
+    name: string
+}
