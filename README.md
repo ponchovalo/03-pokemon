af2f082aec9b18b730b2528b0ea7b4140a52ca60
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-05-06 19:49:08 -0600

    fin seccion 08

--- README.md
+++ README.md
@@ -9,13 +9,17 @@
 ```
 yarn install
 ```
-3. Tener Nest CLI insttalado
+3. Tener Nest CLI instalado
 ```
 npm i -g @nestjs/cli
 ```
 4. Levantar la base de datos
 ```
 docker compose up -d
+```
+5. Reconstruir la base de datos con la semilla
+```
+http://localhost:3000/api/v2/seed
 ```
 
 
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,6 +4,7 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -23,8 +24,17 @@ export class PokemonService {
     }
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll(paginationDto: PaginationDto) {
+
+    const { limit = 10, offSet= 0} = paginationDto;
+
+    return this.pokemonModel.find()
+      .limit(limit)
+      .skip(offSet)
+      .sort({
+        no: 1
+      })
+      .select('-__v')
   }
 
   async findOne(term: string) {
